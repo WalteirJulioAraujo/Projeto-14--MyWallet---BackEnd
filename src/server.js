78d69c4464f0d837baa7933abbb9be1e584ab280
Walteir Araujo <walteiraraujo@poli.ufrj.br>
2021-08-12 02:46:23 -0300

    Acertando a port para o deploy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import "./setup.js";
 import app from "./app.js";
 
-const port = process.env.PORT;
+const port = +process.env.PORT;
 
 app.listen(port,()=>{
     console.log(`Running on port ${port}`);
